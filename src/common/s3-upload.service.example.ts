ebdc7439bac3042ab728a7f73df4275db6a8a5e2
Igor Silva de Souza <igor.silva.ti@gmail.com>
2025-12-14 18:32:02 -0300

    feat(USL-006): Upload de foto de perfil

---
+++ src/common/s3-upload.service.example.ts
@@ -0,0 +1,19 @@
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+export const multerConfig = {
+  storage: diskStorage({
+    destination: './uploads/profile-pictures',
+    filename: (req, file, callback) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      callback(null, `${uniqueSuffix}${extname(file.originalname)}`);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+  fileFilter: (req, file, callback) => {
+    if (!file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
+      return callback(new Error('Only image files are allowed!'), false);
+    }
+    callback(null, true);
+  },
+};
