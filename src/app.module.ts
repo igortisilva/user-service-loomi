43fa237061c11c7f9750ff6f160436f82835cde7
Igor Silva de Souza <igor.silva.ti@gmail.com>
2025-12-14 18:32:46 -0300

    chore(USL-016): Revisão final

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { KafkaModule } from './kafka/kafka.module';
+import { RedisModule } from './redis/redis.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    PrismaModule,
+    UsersModule,
+    AuthModule,
+    KafkaModule,
+    RedisModule,
+  ],
+})
+export class AppModule {}
